377a3dc0cc890677686b34a87fde27ff15c0b353
Bruno-Queiroga <65819487+Bruno-Queiroga@users.noreply.github.com>
2020-08-12 14:39:08 -0300

    Add files via upload

---
+++ Projeto Estudo Java/exemplo-maven/src/test/java/br/com/donthave/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.donthave.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
